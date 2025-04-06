0b260430bd1b3b906acf36563b20326b42f88fa0
George Guerra <george.guerra@tecsup.edu.pe>
2025-04-06 12:35:12 -0500

    Contenedor -LazyColumn

--- app/src/main/java/com/example/controlescontenedores_lab03/MainActivity.kt
+++ app/src/main/java/com/example/controlescontenedores_lab03/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.controlescontenedores_lab03.ui.theme.ControlesContenedoreslab03Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +25,38 @@ class MainActivity : ComponentActivity() {
         setContent {
             ControlesContenedoreslab03Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Box(modifier = Modifier.padding(innerPadding)) {
+                        LazyColumnDemo()
+                    }
                 }
+
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun LazyColumnDemo() {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(200.dp)
+            .padding(8.dp)
+    ) {
+        items(10) { index ->
+            Text(
+                text = "Elemento $index",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ControlesContenedoreslab03Theme {
-        Greeting("Android")
-    }
+fun PreviewLazyColumnDemo() {
+    LazyColumnDemo()
 }
+
