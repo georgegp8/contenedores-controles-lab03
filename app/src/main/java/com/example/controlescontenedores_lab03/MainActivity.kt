446eff3a347c86d64bd4ad3734d906afdd86d80a
George Guerra <george.guerra@tecsup.edu.pe>
2025-04-06 12:41:25 -0500

    Contenedor -LazyRow

--- app/src/main/java/com/example/controlescontenedores_lab03/MainActivity.kt
+++ app/src/main/java/com/example/controlescontenedores_lab03/MainActivity.kt
@@ -9,11 +9,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.controlescontenedores_lab03.ui.theme.ControlesContenedoreslab03Theme
@@ -27,6 +33,7 @@ class MainActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Box(modifier = Modifier.padding(innerPadding)) {
                         LazyColumnDemo()
+                        LazyRowDemo()
                     }
                 }
 
@@ -53,6 +60,40 @@ fun LazyColumnDemo() {
         }
     }
 }
+
+@Composable
+fun LazyRowDemo() {
+    LazyRow(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(100.dp)
+            .padding(8.dp)
+    ) {
+        items(3) { index ->
+            Card(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .width(100.dp),
+                colors = CardDefaults.cardColors(containerColor = Color(0xFFB3E5FC))
+            ) {
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Text("Item $index", modifier = Modifier.padding(8.dp))
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewLazyRowDemo() {
+    LazyRowDemo()
+}
+
+
 
 @Preview(showBackground = true)
 @Composable
