75f3f911319b99d610e5be8bef27787ac81615bf
George Guerra <george.guerra@tecsup.edu.pe>
2025-04-06 13:18:54 -0500

    Contenedor - Scaffold

--- app/src/main/java/com/example/controlescontenedores_lab03/MainActivity.kt
+++ app/src/main/java/com/example/controlescontenedores_lab03/MainActivity.kt
@@ -5,7 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -16,11 +18,20 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -42,7 +53,8 @@ class MainActivity : ComponentActivity() {
                         //LazyColumnDemo()
                         //LazyRowDemo()
                         //GridDemo()
-                        ConstraintLayoutDemo()
+                        //ConstraintLayoutDemo()
+                        ScaffoldDemo()
 
                     }
                 }
@@ -170,6 +182,49 @@ fun ConstraintLayoutDemo() {
         }
     }
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ScaffoldDemo() {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = { Text("Scaffold Demo") }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = {}) {
+                Icon(Icons.Default.Favorite, contentDescription = "FAB")
+            }
+        }
+    ) { innerPadding ->
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            color = Color(0xFFF0F4C3) // Fondo claro (limón suave)
+        ) {
+            Column(
+                modifier = Modifier.padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text("Este es el contenido del cuerpo")
+                Spacer(modifier = Modifier.height(16.dp))
+                Button(onClick = {}) {
+                    Text("Botón dentro del Scaffold")
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewScaffoldDemo() {
+    ScaffoldDemo()
+}
+
+
 
 @Preview(showBackground = true)
 @Composable
